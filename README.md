0dd47ae1b721497219c8f7567779b448799dd7bf
Zhalyaletdinov <zhalayletdinov@gmail.com>
2025-11-08 09:31:05 +0000

    init bundle

---
+++ README.md
@@ -0,0 +1,19 @@
+# Slcorp File Bundle
+
+A clean architecture File bundle for Symfony.
+
+## Installation
+
+This bundle is currently in development and located at `src/Bundles/FileBundle`.
+
+## Configuration
+
+```yaml
+slcorp_file:
+    # Configuration options will be added here
+```
+
+## License
+
+MIT
+
---
+++ src/SlcorpFileBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Slcorp\FileBundle;
+
+use Slcorp\FileBundle\DependencyInjection\SlcorpFileExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @package    SlcorpFileBundle
+ * @copyright  2024 Zhalayletdinov Vyacheslav evil_tut@mail.ru
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class SlcorpFileBundle extends Bundle
+{
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        return new SlcorpFileExtension();
+    }
+}
+
